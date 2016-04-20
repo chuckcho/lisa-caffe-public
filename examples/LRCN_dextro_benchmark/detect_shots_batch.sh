be9c546949a1042cb6e1b66935bb03c4fa880ae2
Chuck Cho <chuck.cho@dextro.co>
2016-04-20 15:57:10 -0400

    Script to batch run shot detections

---
+++ examples/LRCN_dextro_benchmark/detect_shots_batch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SHOTDETECTORBIN=/home/chuck/projects/Shotdetect/build/shotdetect-cmd
+VIDEODIR=/media/6TB/Videos/dextro-benchmark-2016-03-30
+
+for f in ${VIDEODIR}/*.{mp4,mov}; do
+  NAME=${f%.*}
+  BNAME=`basename ${NAME}`_shot
+  echo Processing ${BNAME}
+  mkdir -p ${BNAME} >& /dev/null
+  echo Running: ${SHOTDETECTORBIN} -i ${f} -o ${VIDEODIR}/${BNAME} -s 60 -v -f -a `basename ${NAME}`
+  ${SHOTDETECTORBIN} -i ${f} -o ${VIDEODIR}/${BNAME} -s 60 -v -f -a `basename ${NAME}`
+done
+
+echo -----------------------------------------------------------
+echo Done!
