fb751c58aef232deb3fe305a92c803269f3782c8
Chuck Cho <chuck.cho@dextro.co>
2016-04-21 10:44:08 -0400

    Fix a directory name

--- examples/LRCN_dextro_benchmark/detect_shots_batch.sh
+++ examples/LRCN_dextro_benchmark/detect_shots_batch.sh
@@ -7,7 +7,7 @@ for f in ${VIDEODIR}/*.{mp4,mov}; do
   NAME=${f%.*}
   BNAME=`basename ${NAME}`_shot
   echo Processing ${BNAME}
-  mkdir -p ${BNAME} >& /dev/null
+  mkdir -p ${VIDEODIR}/${BNAME} >& /dev/null
   echo Running: ${SHOTDETECTORBIN} -i ${f} -o ${VIDEODIR}/${BNAME} -s 60 -v -f -a `basename ${NAME}`
   ${SHOTDETECTORBIN} -i ${f} -o ${VIDEODIR}/${BNAME} -s 60 -v -f -a `basename ${NAME}`
 done
